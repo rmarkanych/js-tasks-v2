0df20bb4d2e755d049d2ca615d49854de03be845
Roman <roman.markanych@gmail.com>
2022-08-13 02:07:49 +0300

    add task-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
 import throttle from 'lodash.throttle';
+
+const feedbackForm = document.querySelector('.feedback-form');
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+const messageInput = document.querySelector('textarea[name="message"]');
+const emailInput = document.querySelector('input[name="email"]');
+
+feedbackForm.addEventListener('input', throttle(addToLocalStorage, 500));
+feedbackForm.addEventListener('submit', showLocalStorageData);
+
+let dataObject = {};
+newFun();
+function addToLocalStorage() {
+  dataObject = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(dataObject));
+}
+
+function newFun() {
+  let savedData = localStorage.getItem(LOCALSTORAGE_KEY);
+  let newData = JSON.parse(savedData);
+
+  if (newData) {
+    emailInput.value = newData.email;
+    messageInput.value = newData.message;
+  }
+}
+
+function showLocalStorageData(e) {
+  e.preventDefault();
+  console.log(dataObject);
+  feedbackForm.reset();
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+}
