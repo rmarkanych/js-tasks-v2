d35305342a52f2bfbadd2b5d86b50cf90fc43ed9
Roman <roman.markanych@gmail.com>
2022-08-14 02:57:13 +0300

    add first part 2-task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,10 +2,30 @@ import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('#vimeo-player');
-console.log(iframe);
+const MESSAGE = 'videoplayer-current-time';
 
 const player = new Player(iframe);
 
-player.on('timeupdate', function () {
-  console.log('played the video!');
-});
+player.on(
+  'timeupdate',
+  throttle(data => {
+    localStorage.setItem(MESSAGE, data.seconds);
+  }, 1000)
+);
+
+// player
+//   .setCurrentTime(30.456)
+//   .then(function (seconds) {
+//     // seconds = the actual time that the player seeked to
+//   })
+//   .catch(function (error) {
+//     switch (error.name) {
+//       case 'RangeError':
+//         // the time was less than 0 or greater than the video’s duration
+//         break;
+
+//       default:
+//         // some other error occurred
+//         break;
+//     }
+//   });
