ded0ed89cc396b672ce0e8ba354bec8ed7652968
Roman <roman.markanych@gmail.com>
2022-08-14 00:40:21 +0300

    add task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,2 +1,11 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('#vimeo-player');
+console.log(iframe);
+
+const player = new Player(iframe);
+
+player.on('timeupdate', function () {
+  console.log('played the video!');
+});
